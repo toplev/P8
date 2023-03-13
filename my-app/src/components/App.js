f7bd0818535c06994b4efc0a8d77d59bb5d371de
toplev <topourialevan@gmail.com>
2023-03-13 21:57:47 +0100

    Étape 1 : Initialisez le projet

---
+++ my-app/src/components/App.js
@@ -0,0 +1,14 @@
+import Banner from './Banner'
+import RealEstateList from './RealEstateList'
+
+
+function App() {
+	return (
+		<div>
+			<Banner />
+			<RealEstateList />
+		</div>
+	)
+}
+
+export default App
---
+++ my-app/src/components/RealEstateList.js
@@ -0,0 +1,30 @@
+import { aptoList } from '../datas/aptoList'
+import '../styles/ShoppingList.css'
+
+function RealEstateList() {
+	const categories = aptoList.reduce(
+		(acc, apto) =>
+			acc.includes(apto.category) ? acc : acc.concat(apto.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{aptoList.map((apto) => (
+					<li key={apto.id} className='lmj-plant-item'>
+						{apto.title}
+						{apto.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default RealEstateList
