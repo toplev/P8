4331b81decda487ec03db15ec5187b2a38f93e54
toplev <topourialevan@gmail.com>
2023-03-26 00:12:57 +0100

    apto update

--- my-app/src/components/Apto.js
+++ my-app/src/components/Apto.js
@@ -1,20 +1,35 @@
+import '../styles/Apto.css'
 import { aptoList } from '../datas/aptoList'
 import { useParams } from "react-router-dom";
 
+
 const Service = () => {
 	const idinlist = useParams ('id').id;
-	console.log(idinlist)
 	const idnow = aptoList.filter(data => data.id === idinlist)
+	const numbers = [1, 2, 3, 4, 5]
+	const doubles = numbers.map(x => x * 2) 
 
 	return (
 		<div>
-			<ul className='lmj-plant-list'>
+			<div className='lmj-plant-list'>
 			<img src={idnow[0].cover} alt='Kasa Logo' className="apto-img"/>
-			<h3>{idnow[0].title}</h3>
-			</ul>
+			
+			<div>
+			<h3 className="apto-text">{idnow[0].title}</h3>
+			<h1 className="apto-text2">{idnow[0].location}</h1>
+			<h1 className="apto-text2">{idnow[0].host.name}</h1>
+			<img src={idnow[0].host.picture} alt='Kasa Logo' className="apto-img2"/>
+			<h1 className="apto-text2">{idnow[0].rating}</h1>
+			{idnow[0].rating ? <span>🔥</span> : <span>👎</span>}
+			<h1 className="apto-text2">{numbers}</h1>
+			<h1 className="apto-text2">{doubles}</h1>
+
+			<div>
+        </div>
+			</div>
+			</div>
 		</div>
 	)
 }
 
 export default Service
-
--- my-app/src/components/Gallery.js
+++ my-app/src/components/Gallery.js
@@ -2,14 +2,18 @@ import '../styles/Gallery.css'
 import { aptoList } from '../datas/aptoList'
 import { Link } from 'react-router-dom'
 
+
 function Gallery() {
 	return (
 			<div className='gallerybackgroud'>
 				{aptoList.map((apto) => (
 					<div key={apto.id} className='apto-list'>
-						<Link to={`/Apto/${apto.id}`}><img src={apto.cover} alt='Kasa Logo' className="apto-img"/></Link>
+						<div>
+						<Link to={`/Apto/${apto.id}`}><img src={apto.cover} alt='Kasa Logo' className="gradient-img"/></Link>
+						</div>
 						<div className='Gallery2'>{apto.title}</div>
 					</div>
+
 				))}
 			</div>
 	)
