22d5105f309749332ff8705711f5e408ebbd0e81
toplev <topourialevan@gmail.com>
2023-03-26 22:02:44 +0200

    apto update

--- my-app/src/components/Apto.js
+++ my-app/src/components/Apto.js
@@ -17,21 +17,23 @@ const Service = () => {
 
 	return (
 		<div>
-			<div className='lmj-plant-list'>
+			<div className='aptolist'>
 			<img src={idnow[0].cover} alt='Kasa Logo' className="apto-img"/>
 			<div>
+			<div className='info'>
+			<div className='infoleft'>
 			<h3 className="apto-text">{idnow[0].title}</h3>
 			<h1 className="apto-text2">{idnow[0].location}</h1>
-			<h1 className="apto-text2">{idnow[0].host.name}</h1>
+			<div>{idnow[0].tags.map((tag, index) => (<button key={index}className="button-text2">{tag}</button>))}</div>
+			</div>
+			<div className='inforight'>
+			<h1 className="apto-text3">{idnow[0].host.name}</h1>
 			<img src={idnow[0].host.picture} alt='Kasa Logo' className="apto-img2"/>
-			{getStarRating().map((star, index) => (
-  <span key={index} className="star-rating">{star}</span>
-))}			<div>
-
-{idnow[0].tags.map((tag, index) => (
-  <button key={index}className="button-text2">{tag}</button>
-))}			</div>
-
+			<div>{getStarRating().map((star, index) => (
+ 			 <span key={index} className="star-rating">{star}</span>))}
+			 </div>		
+			 </div>		
+			</div>
 			</div>
 			</div>
 		</div>
